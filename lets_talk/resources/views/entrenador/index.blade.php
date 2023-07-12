e749048926cfd4a76069ff4e5b1db8801b062e6e
Juguimeco <jgmejiaco@gmail.com>
2023-07-12 17:54:33 -0500

    botones save y old evaluation session details

--- lets_talk/resources/views/entrenador/index.blade.php
+++ lets_talk/resources/views/entrenador/index.blade.php
@@ -358,16 +358,10 @@
 
                     Swal.fire({
                         html: html,
-                        showCloseButton: true,
-                        showCancelButton: true,
+                        showCloseButton: false,
+                        showCancelButton: false,
                         focusConfirm: false,
                         allowOutsideClick: false,
-                        confirmButtonText:
-                            '<i class="fa fa-thumbs-up"></i> Great!',
-                        confirmButtonAriaLabel: 'Thumbs up, great!',
-                        cancelButtonText:
-                            '<i class="fa fa-thumbs-down"></i>',
-                        cancelButtonAriaLabel: 'Thumbs down',
                         width: 850,
                         padding: '5em',
                         background: '#fff',
