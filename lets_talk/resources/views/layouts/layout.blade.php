36d0f62c0d0ff084fff02be45300f781419a6fd7
jgmejiaco <jgmejiaco@gmail.com>
2023-08-09 12:48:18 -0500

    el footer ya queda posicionado en el fondo de la pagina

--- lets_talk/resources/views/layouts/layout.blade.php
+++ lets_talk/resources/views/layouts/layout.blade.php
@@ -5,29 +5,30 @@
     @include('layouts.header')
 </head>
 <body>
-<div class="container">
-    <div class="row">
-        <div class="col-xs-6 col-sm-6 col-md-6">
-            <div class="logo-box">
-                <img src="{{asset('img/logo.png')}}" alt="logo" class="logo logo-img">
-            </div>
-        </div>
-
-        @if(Request()->path() == '/' || Request()->path() == "login" ||
-            Request()->path() == "login_estudiante")
+    <div class="container" style="margin-bottom: 5em;">
+        <div class="row">
             <div class="col-xs-6 col-sm-6 col-md-6">
-                <div class="sign-out">
-                    &nbsp;
+                <div class="logo-box">
+                    <img src="{{asset('img/logo.png')}}" alt="logo" class="logo logo-img">
                 </div>
             </div>
-        @else
-            @include('layouts.menu')
-        @endif
+
+            @if(Request()->path() == '/' || Request()->path() == "login" ||
+                Request()->path() == "login_estudiante")
+                <div class="col-xs-6 col-sm-6 col-md-6">
+                    <div class="sign-out">
+                        &nbsp;
+                    </div>
+                </div>
+            @else
+                @include('layouts.menu')
+            @endif
+        </div>
+
+        @yield('content')
+
     </div>
 
-    @yield('content')
-
-    </div>
         @include('layouts.footer')
 </body>
 </html>
