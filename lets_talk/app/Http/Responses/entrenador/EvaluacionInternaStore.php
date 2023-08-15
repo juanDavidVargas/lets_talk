5236298e801d88c691323b4d0e560d02c641b1a9
jgmejiaco <jgmejiaco@gmail.com>
2023-08-15 10:05:29 -0500

    se crean los archivos responsables de la evaluacion interna y se migran los metodos del CRUD a los nuevos archivos

---
+++ lets_talk/app/Http/Responses/entrenador/EvaluacionInternaStore.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Responses\entrenador;
+
+use App\User;
+use Exception;
+use Illuminate\Contracts\Support\Responsable;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+use Illuminate\Log\Logger;
+use App\Models\entrenador\EvaluacionInterna;
+
+class EvaluacionInternaStore implements Responsable
+{
+    public function toResponse($request)
+    {
+        $evaluacionInterna = request('evaluacion_interna', null);
+        $idEstudiante = request('id_estudiante', null);
+        // $idInstructor = request('id_instructor', null); // Se habilita al estar listo el módulo de estudiante
+
+        DB::connection('mysql')->beginTransaction();
+
+        try {
+            $evaluacionInternaCreate = EvaluacionInterna::create([
+                'evaluacion_interna' => $evaluacionInterna,
+                'id_estudiante' => $idEstudiante,
+                'id_instructor' => 7,
+            ]);
+
+            if ($evaluacionInternaCreate) {
+                DB::connection('mysql')->commit();
+                alert()->success('Successful Process', 'Internal valuation created');
+                return redirect()->to(route('trainer.index'));
+            } else {
+                DB::connection('mysql')->rollback();
+                alert()->error('Error', 'An error has occurred creating the user, please contact support.');
+                return redirect()->to(route('entrenador.index'));
+            }
+
+        } catch (Exception $e) {
+            DB::connection('mysql')->rollback();
+            alert()->error('Error', 'An error has occurred creating the user, try again, if the problem persists contact support.');
+            return back();
+        }
+    }
+}
