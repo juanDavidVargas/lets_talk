72f9983d2cf503ac2986e216a438f6940944a17e
jgmejiaco <jgmejiaco@gmail.com>
2023-08-18 10:56:32 -0500

    Se cambia el idioma español a ingles para el mensaje de falla de conexion a la BD

--- lets_talk/resources/views/database_connection.blade.php
+++ lets_talk/resources/views/database_connection.blade.php
@@ -7,6 +7,6 @@
 {{-- ===================================== --}}
 
 @section('content')
-    <h3 style="margin-bottom: 1em;">Estado de conexión a la base de datos</h3>
-    <p style="font-size: 18px">No se pudo conectar a la base de datos.</p>
+    <h3 style="margin-bottom: 1em;">Data Base Connection State</h3>
+    <p style="font-size: 18px">It wasn't possible to connect to the database.</p>
 @stop
