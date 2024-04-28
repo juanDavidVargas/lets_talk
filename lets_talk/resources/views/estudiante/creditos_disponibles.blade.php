6e161c85a74e128c5f9cd437b8f8f89f022f8a8a
Juangui <jgmejiaco@gmail.com>
2024-04-28 13:46:16 -0500

    inicio implementación modulo mis creditos

---
+++ lets_talk/resources/views/estudiante/creditos_disponibles.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.layout')
+@section('title', 'Mis Créditos')
+@section('css')
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <h2 class="text-center text-uppercase">Mis créditos</h2>
+        </div>
+    </div>
+
+    <div class="row p-b-20 float-left">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <a href="{{route('estudiante.index')}}" class="btn btn-primary text-uppercase">Atrás Disponibilidad</a>
+        </div>
+    </div>
+
+    {{-- <hr class="border border-1 border-secondary p-0 rounded-0"> --}}
+
+    <div class="row border w-75 ms-auto me-auto mt-5 mb-5 p-5">
+        <div class="col-12 col-md-4">
+            fecha de la compra
+        </div>
+
+        <div class="col-12 col-md-4">
+            6 de marzo
+        </div>
+
+        <div class="col-12 col-md-4">
+            cantidad: 5
+        </div>
+    </div>
+
+    <div class="d-flexswal m-t-30 m-b-30">
+        {{-- <button type="button" class="btn btn-primary text-uppercase" onclick="misCreditos()">10 créditos disponibles</button> --}}
+        <b href="{{route('estudiante.creditos_disponibles')}}" class="btn btn-primary text-uppercase">10 créditos disponibles</b>
+    </div>
+
+    <div class="d-flex justify-content-center">
+        <b href="https://www.pse.com.co/persona" target="_blank" class="btn btn-primary text-uppercase">comprar más créditos</b>
+    </div>
+
+    @include('layouts.loader')
+@stop
+
+@section('scripts')
+    <script>
+
+    function misCreditos() {
+        Swal.fire(
+            'Info',
+            'mis creditos.',
+            'info'
+        );
+    }
+
+
+        
+    </script>
+@endsection
