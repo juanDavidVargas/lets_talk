b045404a505a41e43489053be47b15a0a83caf1f
Juangui <jgmejiaco@gmail.com>
2024-05-06 12:00:50 -0500

    creación de modelos de reservas y creditos

---
+++ lets_talk/app/Models/usuarios/Reserva.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models\usuarios;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Reserva extends Model
+{
+    use SoftDeletes;
+
+    protected $connection = 'mysql';
+    protected $table = 'reservas';
+    protected $primaryKey = 'id_reserva';
+    protected $dates = ['deleted_at'];
+    public $timestamps = true;
+    protected $fillable = [
+       'id_estudiante',
+       'id_instructor',
+       'id_horario',
+    ];
+}
